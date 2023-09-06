011eed665cc7b754d6a90d1f644977a2f5afa9c5
class_zou <2359105796@qq.com>
2023-09-06 21:15:36 +0800

    netty实现websocket

---
+++ mallchat-chat-server/src/main/java/com/ztl/mallchat/common/MallchatCustomApplication.java
@@ -0,0 +1,16 @@
+package com.ztl.mallchat.common;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication(scanBasePackages = {"com.ztl.mallchat"})
+@ServletComponentScan
+public class MallchatCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallchatCustomApplication.class,args);
+    }
+
+}
