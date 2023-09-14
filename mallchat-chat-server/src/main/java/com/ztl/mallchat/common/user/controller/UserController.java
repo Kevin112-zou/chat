7dcfe499c8167d219c5d9d975dc12bad3eb06fb6
JefferyRui <lvqingrui001012@163.com>
2023-09-15 01:28:17 +0800

    优化swagger文档

--- mallchat-chat-server/src/main/java/com/ztl/mallchat/common/user/controller/UserController.java
+++ mallchat-chat-server/src/main/java/com/ztl/mallchat/common/user/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.ztl.mallchat.common.user.controller;
 
 
+import com.ztl.mallchat.common.user.domain.vo.response.user.UserInfoResp;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -13,9 +18,14 @@ import org.springframework.stereotype.Controller;
  * @author kevin
  * @since 2023-09-08
  */
-@Controller
+@RestController
 @RequestMapping("/user")
+@Api("用户相关接口")
 public class UserController {
-
+    @GetMapping("/userInfo")
+    @ApiOperation("用户详情")
+    public UserInfoResp getUserInfo() {
+        return null;
+    }
 }
 
